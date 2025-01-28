39adfaaa37dd9a2e858d44040f6a83d1ed68d26b
Pedro Siqueira <pedro.siqueira1999@hotmail.com>
2025-01-27 22:27:08 -0300

    refazendo o navbar

--- lib/ui/home/widgets/home_screen.dart
+++ lib/ui/home/widgets/home_screen.dart
@@ -9,47 +9,70 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Watch((context) {
-        return controller.page[controller.selectedScreenIndex.value];
-      }),
-      bottomNavigationBar: Watch((context) {
-        return BottomNavigationBar(
-          currentIndex: controller.selectedScreenIndex.value,
-          onTap: controller.onTapScreen,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-              ),
-              label: 'Popular',
+      body: Stack(
+        children: [
+          Watch((context) {
+            return controller.page[controller.selectedScreenIndex.value];
+          }),
+          BottomNav(
+              itemCount: controller.page.length,
+              items: [
+                Icon(Icons.favorite),
+                Icon(Icons.favorite),
+              ],
+              onItemTap: controller.onTapScreen),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNav extends StatelessWidget {
+  final int itemCount;
+  final void Function(int) onItemTap;
+  final List<Widget> items;
+  const BottomNav(
+      {super.key,
+      required this.itemCount,
+      required this.onItemTap,
+      required this.items});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 10,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Container(
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.movie,
-              ),
-              label: 'Estreia',
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.home),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.favorite),
+                  onPressed: () {},
+                ),
+              ],
             ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.search,
-              ),
-              label: 'Buscar',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.ac_unit,
-              ),
-              label: 'Top filmes',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.favorite,
-              ),
-              label: 'Favoritos',
-            ),
-          ],
-        );
-      }),
+          ),
+        ),
+      ),
     );
   }
 }
